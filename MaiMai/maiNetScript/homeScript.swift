282aaf427ea7cb7b892cca25f7e194672ac9d32b
Jethro.Wang <jethro.wang@bomdic.com>
2023-03-16 11:12:34 +0800

    change name and structure

---
+++ MaiMai/maiNetScript/homeScript.swift
@@ -0,0 +1,38 @@
+//
+//  maimaiScript.swift
+//  MaiMai
+//
+//  Created by Yukino on 2023/3/16.
+//
+
+import Foundation
+
+
+func homeScript(webView: WKWebView){
+    
+    /**
+     Used for collecting players data such as playerID, rating, title
+     **/
+    
+    // var userInfo: UserInfo = UserInfo()
+    
+    
+    /**
+     - query :
+     The Selector of the element to find :
+     By ID: ('#idName')
+     By Class Name: ('.className')
+     By Element type: ('elementType') (e.g. 'div', 'span', 'a', etc.)
+     By compound selector: ('element.class') (e.g. 'class1.class2', 'a.external', '.red.bold', etc.)
+     By CSS selector: ('selector') (e.g. '#id .class element[attr="value"]', etc.)
+     By attribute: ('[attribute="value"]') (e.g. '[name="username"]', '[data-id="123"]', etc.)
+     **/
+    
+    webView.evaluateJavaScript("document.querySelector('.name_block.f_l.f_16').innerText") { (result, error) in
+        let name = result as? String
+    }
+    
+    webView.evaluateJavaScript("document.querySelector('[name=\"rating_block\"]').innerText") { (result, error) in
+        let rating = result as? String
+    }
+}
--- MaiMai/maiNetScript/loginScript.swift
+++ MaiMai/maiNetScript/loginScript.swift
@@ -1,11 +1,12 @@
 //
-//  maimaiScript.swift
+//  loginScript.swift
 //  MaiMai
 //
 //  Created by Yukino on 2023/3/16.
 //
 
 import Foundation
+
 import WebKit
 
 func loginScript(webView: WKWebView){
@@ -43,33 +44,4 @@ func loginScript(webView: WKWebView){
             loginPress.click();
     """
     )
-}
-
-func homeScript(webView: WKWebView){
-    
-    /**
-     Used for collecting players data such as playerID, rating, title
-     **/
-    
-    // var userInfo: UserInfo = UserInfo()
-    
-    
-    /**
-     - query :
-     The Selector of the element to find :
-     By ID: ('#idName')
-     By Class Name: ('.className')
-     By Element type: ('elementType') (e.g. 'div', 'span', 'a', etc.)
-     By compound selector: ('element.class') (e.g. 'class1.class2', 'a.external', '.red.bold', etc.)
-     By CSS selector: ('selector') (e.g. '#id .class element[attr="value"]', etc.)
-     By attribute: ('[attribute="value"]') (e.g. '[name="username"]', '[data-id="123"]', etc.)
-     **/
-    
-    webView.evaluateJavaScript("document.querySelector('.name_block.f_l.f_16').innerText") { (result, error) in
-        let name = result as? String
-    }
-    
-    webView.evaluateJavaScript("document.querySelector('[name=\"rating_block\"]').innerText") { (result, error) in
-        let rating = result as? String
-    }
 }
